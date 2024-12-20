7435db84fa64b36a1f10a199b156c88349a2567f
Ahmad Surya Alam Syah <alamsyah200527@gmail.com>
2024-12-20 23:13:44 +0700

    route 2

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\FrontController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\WalletTransactionController;
 use Illuminate\Support\Facades\Route;
 use SebastianBergmann\CodeCoverage\Report\Html\Dashboard;
 
@@ -24,9 +26,24 @@ Route::middleware('auth')->group(function () {
         Route::post('/dashboard/wallet/withdraw/store', [DashboardController::class, 'withdraw_wallet_store'])->name('dashboard.wallet.withdraw.store');
     });
 
-    Route::group('can:topup wallet', function () {
+    Route::middleware('can:topup wallet')->group(function () {
         Route::get('/dashboard/wallet/topup', [DashboardController::class, 'topup_wallet'])->name('dashboard.wallet.topup');
         Route::post('/dashboard/wallet/topup/store', [DashboardController::class, 'topup_wallet_store'])->name('dashboard.wallet.topup.store');
+    });
+
+    Route::middleware('can:apply job')->group(function () {
+        Route::get('/apply/{project:slug}', [FrontController::class, 'apply_job'])->name('front.apply_job');
+        Route::post('/apply/{project:slung}/submit', [FrontController::class, 'applay_job_store'])->name('front.apply_job.store');
+        Route::get('/dashboard/proposals', [DashboardController::class, 'proposals'])->name('dashboard.proposals');
+        Route::get('/dashboard/proposal_details/{project}/{projectApplicant}', [DashboardController::class, 'proposal_details'])->name('dashboard.proposal_details');
+    });
+
+    Route::prefix('admin')->name('admin.')->group(function () {
+        Route::middleware('can:manage wallets')->group(function () {
+            Route::get('/wallet/topups', [WalletTransactionController::class, 'wallet_topups'])->name('topups');
+            Route::get('/wallet/withdrawals', [WalletTransactionController::class, 'wallet_withdrawals'])->name('withdrawals');
+            Route::resource('wallet_transactions', WalletTransactionController::class);
+        });
     });
 
 });
